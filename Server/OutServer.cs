c7b04544b174d13990af8ac9519788e0890c533c
Даниил <geimer_cok@mail.ru>
2023-01-17 22:18:19 +0300

    да не спорю с Client'om у меня не получилось

---
+++ Server/OutServer.cs
@@ -0,0 +1,91 @@
+// using System.Net.Sockets;
+// using System.Net;
+// using System.Text;
+
+// namespace Server
+// {
+//     class OutServer
+//     {
+//         TcpListener server;
+
+//         public OutServer()
+//         {
+//             server = new TcpListener(IPAddress.Parse("127.0.0.1"), 5555);
+//             server.Start();
+
+//             LoopClients();
+//         }
+
+//         void LoopClients()
+//         {
+//             while(true)
+//             {
+//                 TcpClient client = server.AccepTcpClient();
+
+//                 Thread thread = new Thread(()=> HandleClient(client));
+//                 thread.Start();
+//             }
+//         }
+        
+//         void HandleClient(TcpClient client)
+//         {
+//             StreamReader sReader = new StreamReader(LoopClients.GetStream(), Encoding.UTF8);
+//             //StreamReader sWriter = new StreamWriter(LoopClients.GetStream(), Encoding.UTF8);
+
+//             while (true)
+//             {
+//                 string massege = aReader.ReadLine();
+//                 System.Console.WriteLine($"client wrote -{massege}");
+//             }
+//         }
+//     }
+// }
+
+
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+namespace Server
+{
+    class OurServer
+    {
+        TcpListener server;
+
+        public OurServer()
+        {
+            server = new TcpListener(IPAddress.Parse("127.0.0.1"), 5555);
+            server.Start();
+
+            LoopClients();  
+        }
+
+        void LoopClients()
+        {
+            while (true)
+            {
+                TcpClient client = server.AcceptTcpClient();
+
+                Thread thread = new Thread(() => HandleClient(client));
+                thread.Start();
+            }
+        }
+
+        void HandleClient(TcpClient client)
+        {
+            StreamReader sReader = new StreamReader(client.GetStream(), Encoding.UTF8);
+            StreamWriter sWriter = new StreamWriter(client.GetStream(), Encoding.UTF8);
+
+            while (true)
+            {
+                string message = sReader.ReadLine();
+                Console.WriteLine($"Клиент написал - {message}");
+
+                Console.WriteLine("Дайте сообщение клиенту: ");
+                string answer = Console.ReadLine();
+                sWriter.WriteLine(answer);
+                sWriter.Flush();
+            }
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,23 @@
+﻿// namespace Server
+// {
+//     class program
+//     {
+//         static void Main(string[] args)
+//         {
+//             System.Console.WriteLine("This is our server");
+//             OutServer server = new OutServer();
+//         }
+//     }
+// }
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Это наш сервер");
+            OurServer server = new OurServer();
+        }
+    }
+}
